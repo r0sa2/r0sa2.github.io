80f03b26629757b3dbfaa2292de3b35d9e594252
r0sa2 <rohan.saraogi@gmail.com>
2026-05-21 18:22:53 +0530

    Added Github action; modified README and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://r0sa2.github.io'
+});
